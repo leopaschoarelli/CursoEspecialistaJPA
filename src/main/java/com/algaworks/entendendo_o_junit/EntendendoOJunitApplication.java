cdeea78d2b0eeb9cfb6b347b738dc13da93aa9c0
leo95h <leonardo.lhpr@gmail.com>
2024-06-29 13:18:41 -0300

    Capitulo 1 Aula 09 - Entendendo o JUnit

---
+++ src/main/java/com/algaworks/entendendo_o_junit/EntendendoOJunitApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.entendendo_o_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntendendoOJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntendendoOJunitApplication.class, args);
+	}
+
+}
